f7936891a1e02c450a134849dc030f2b2da896af
hp <hp@DESKTOP-SNCN38L>
2022-03-28 16:39:50 +0530

    my xyz files

---
+++ com.crm.xyz.demo/src/test/java/com/crm/demo_0/Demo_000_Test.java
@@ -0,0 +1,22 @@
+package com.crm.demo_0;
+
+import org.testng.annotations.Test;
+
+public class Demo_000_Test {
+	@Test
+	public void m1() {
+		System.out.println("m1 run");
+	}
+	@Test
+	public void m2() {
+		System.out.println("m2 run");
+	}
+	@Test
+	public void m3() {
+		System.out.println("m3 run");
+	}
+	@Test
+	public void m4() {
+		System.out.println("m4 run");
+	}
+}
---
+++ com.crm.xyz.demo/src/test/java/com/crm/demo_0/Demo_00_Test.java
@@ -0,0 +1,23 @@
+package com.crm.demo_0;
+
+import org.testng.annotations.Test;
+
+public class Demo_00_Test {
+	
+	@Test
+	public void n1() {
+		System.out.println("n1 run");
+	}
+	@Test
+	public void n2() {
+		System.out.println("n2 run");
+	}
+	@Test
+	public void n3() {
+		System.out.println("n3 run");
+	}
+	@Test
+	public void n4() {
+		System.out.println("n4 run");
+	}
+}
